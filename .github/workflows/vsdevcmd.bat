cd9111a067b4b59c4734a90cff9444561aab5c9a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-08-16 11:10:54 +0300

    a

--- .github/workflows/vsdevcmd.bat
+++ .github/workflows/vsdevcmd.bat
@@ -1,7 +1,8 @@
 @if not defined _echo echo off
 for /f "usebackq delims=" %%i in (`vswhere.exe -prerelease -latest -property installationPath`) do (
   if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-    %comspec% /k "%%i\Common7\Tools\vsdevcmd.bat" %*
+    rem %comspec% /k 
+	"%%i\Common7\Tools\vsdevcmd.bat" %*
     rem exit /b
   )
 )
